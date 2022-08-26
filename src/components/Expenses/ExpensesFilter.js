08ae92a13583b6e866dda5e4e68863eeb31b02a8
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-26 11:30:27 +0200

    Implement Expenses Filter

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,15 +1,15 @@
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = (props) => {
-  const dropdownChangeHandler = (event) => {
-    props.onChangeFilter(event.target.value);
+const ExpensesFilter = ({ onChangeFilter, selected }) => {
+  const dropdownChangeHandler = (e) => {
+    onChangeFilter(e.target.value);
   };
 
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select value={props.selected} onChange={dropdownChangeHandler}>
+        <select value={selected} onChange={dropdownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
